57d4953a50129c7a51f58a512328865c5878665e
Max Brunsfeld <mbrunsfeld@pivotallabs.com>
2014-04-21 16:14:49 -0700

    bump deps
    Signed-off-by: Alex Suraci <asuraci@pivotallabs.com>

--- Godeps/_workspace/src/github.com/gorilla/websocket/conn_test.go
+++ Godeps/_workspace/src/github.com/gorilla/websocket/conn_test.go
@@ -1,4 +1,4 @@
-// Copyright 2013 Gary Burd. All rights reserved.
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -15,7 +15,7 @@ import (
 	"time"
 )
 
-var timeoutErrImplementsNetError net.Error = errWriteTimeout
+var _ net.Error = errWriteTimeout
 
 type fakeNetConn struct {
 	io.Reader
