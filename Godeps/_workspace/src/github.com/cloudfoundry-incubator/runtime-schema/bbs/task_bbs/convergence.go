45798980aa6f34d49d9460ad946717c29c53256f
Onsi Fakhouri <onsi@pivotallabs.com>
2014-05-22 10:12:45 -0700

    update deps

--- Godeps/_workspace/src/github.com/cloudfoundry-incubator/runtime-schema/bbs/task_bbs/convergence.go
+++ Godeps/_workspace/src/github.com/cloudfoundry-incubator/runtime-schema/bbs/task_bbs/convergence.go
@@ -11,13 +11,13 @@ import (
 
 // ConvergeTask is run by *one* executor every X seconds (doesn't really matter what X is.. pick something performant)
 // Converge will:
-// 1. Kick (by setting) any run-onces that are still pending
-// 2. Kick (by setting) any run-onces that are completed
+// 1. Kick (by setting) any run-onces that are still pending (and have been for > convergence interval)
+// 2. Kick (by setting) any run-onces that are completed (and have been for > convergence interval)
 // 3. Demote to pending any claimed run-onces that have been claimed for > 30s
 // 4. Demote to completed any resolving run-onces that have been resolving for > 30s
 // 5. Mark as failed any run-onces that have been in the pending state for > timeToClaim
 // 6. Mark as failed any claimed or running run-onces whose executor has stopped maintaining presence
-func (self *TaskBBS) ConvergeTask(timeToClaim time.Duration) {
+func (self *TaskBBS) ConvergeTask(timeToClaim time.Duration, convergenceInterval time.Duration) {
 	taskState, err := self.store.ListRecursively(shared.TaskSchemaRoot)
 	if err != nil {
 		return
@@ -38,7 +38,6 @@ func (self *TaskBBS) ConvergeTask(timeToClaim time.Duration) {
 	}
 
 	keysToDelete := []string{}
-	unclaimedTimeoutBoundary := self.timeProvider.Time().Add(-timeToClaim).UnixNano()
 
 	tasksToCAS := []compareAndSwappableTask{}
 	scheduleForCASByIndex := func(index uint64, newTask models.Task) {
@@ -59,22 +58,24 @@ func (self *TaskBBS) ConvergeTask(timeToClaim time.Duration) {
 			continue
 		}
 
+		shouldKickTask := self.durationSinceTaskUpdated(task) >= convergenceInterval
+
 		switch task.State {
 		case models.TaskStatePending:
-			if task.CreatedAt <= unclaimedTimeoutBoundary {
+			shouldMarkAsFailed := self.durationSinceTaskCreated(task) >= timeToClaim
+			if shouldMarkAsFailed {
 				logError(task, "task.converge.failed-to-claim")
 				scheduleForCASByIndex(node.Index, markTaskFailed(task, "not claimed within time limit"))
-			} else {
+			} else if shouldKickTask {
 				scheduleForCASByIndex(node.Index, task)
 			}
 		case models.TaskStateClaimed:
-			claimedTooLong := self.timeProvider.Time().Sub(time.Unix(0, task.UpdatedAt)) >= 30*time.Second
 			_, executorIsAlive := executorState.Lookup(task.ExecutorID)
 
 			if !executorIsAlive {
 				logError(task, "task.converge.executor-disappeared")
 				scheduleForCASByIndex(node.Index, markTaskFailed(task, "executor disappeared before completion"))
-			} else if claimedTooLong {
+			} else if shouldKickTask {
 				logError(task, "task.converge.failed-to-start")
 				scheduleForCASByIndex(node.Index, demoteToPending(task))
 			}
@@ -86,11 +87,11 @@ func (self *TaskBBS) ConvergeTask(timeToClaim time.Duration) {
 				scheduleForCASByIndex(node.Index, markTaskFailed(task, "executor disappeared before completion"))
 			}
 		case models.TaskStateCompleted:
-			scheduleForCASByIndex(node.Index, task)
+			if shouldKickTask {
+				scheduleForCASByIndex(node.Index, task)
+			}
 		case models.TaskStateResolving:
-			resolvingTooLong := self.timeProvider.Time().Sub(time.Unix(0, task.UpdatedAt)) >= 30*time.Second
-
-			if resolvingTooLong {
+			if shouldKickTask {
 				logError(task, "task.converge.failed-to-resolve")
 				scheduleForCASByIndex(node.Index, demoteToCompleted(task))
 			}
@@ -99,6 +100,14 @@ func (self *TaskBBS) ConvergeTask(timeToClaim time.Duration) {
 
 	self.batchCompareAndSwapTasks(tasksToCAS, logger)
 	self.store.Delete(keysToDelete...)
+}
+
+func (self *TaskBBS) durationSinceTaskCreated(task models.Task) time.Duration {
+	return self.timeProvider.Time().Sub(time.Unix(0, task.CreatedAt))
+}
+
+func (self *TaskBBS) durationSinceTaskUpdated(task models.Task) time.Duration {
+	return self.timeProvider.Time().Sub(time.Unix(0, task.UpdatedAt))
 }
 
 func markTaskFailed(task models.Task, reason string) models.Task {
