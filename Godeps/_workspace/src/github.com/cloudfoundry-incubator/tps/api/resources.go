9f4c2582ef03bdffc9a1ade7c9bc7806b05b58ba
Abhijit Hiremagalur <abhi@pivotallabs.com>
2014-09-16 14:00:00 -0700

    Remove godeps; vendor in diego-release instead
    [#78292566]
    
    Signed-off-by: Matthew Sykes <matthew.sykes@gmail.com>

--- Godeps/_workspace/src/github.com/cloudfoundry-incubator/tps/api/resources.go
+++
@@ -1,9 +0,0 @@
-package api
-
-type LRPInstance struct {
-	ProcessGuid  string `json:"process_guid"`
-	InstanceGuid string `json:"instance_guid"`
-	Index        uint   `json:"index"`
-	State        string `json:"state"`
-	Since        int64  `json:"since_in_ns"`
-}
