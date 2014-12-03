8395886d460d024caf0c12485b44c50a2a45b9fc
Alex Suraci <asuraci@pivotal.io>
2014-12-03 12:28:58 -0800

    add logging to garden cleanup

--- helpers/cleanup_garden.go
+++ helpers/cleanup_garden.go
@@ -1,9 +1,11 @@
 package helpers
 
 import (
+	"fmt"
 	"strings"
 
 	garden "github.com/cloudfoundry-incubator/garden/api"
+	"github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
@@ -11,10 +13,16 @@ func CleanupGarden(gardenClient garden.Client) []error {
 	containers, err := gardenClient.Containers(nil)
 	Ω(err).ShouldNot(HaveOccurred())
 
+	fmt.Fprintf(ginkgo.GinkgoWriter, "cleaning up %d Garden containers", len(containers))
+
 	// even if containers fail to destroy, stop garden, but still report the
 	// errors
 	destroyContainerErrors := []error{}
 	for _, container := range containers {
+		info, _ := container.Info()
+
+		fmt.Fprintf(ginkgo.GinkgoWriter, "cleaning up container %s (%s)", container.Handle(), info.ContainerPath)
+
 		err := gardenClient.Destroy(container.Handle())
 		if err != nil {
 			if !strings.Contains(err.Error(), "unknown handle") {
