beef14bf6915366cb1cf974bc3e17fc46dce0d82
Amit Gupta and Leo Chu <leocchu@gmail.com>
2015-05-27 11:26:22 -0700

    garden cleanup helpers retry destroying

--- helpers/cleanup_garden.go
+++ helpers/cleanup_garden.go
@@ -3,6 +3,7 @@ package helpers
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/cloudfoundry-incubator/garden"
 	"github.com/onsi/ginkgo"
@@ -23,17 +24,27 @@ func CleanupGarden(gardenClient garden.Client) []error {
 
 		fmt.Fprintf(ginkgo.GinkgoWriter, "cleaning up container %s (%s)", container.Handle(), info.ContainerPath)
 
-		err := gardenClient.Destroy(container.Handle())
-		if err != nil {
-			if strings.Contains(err.Error(), "unknown handle") {
+		// try to Destroy the container up to 3 times
+		for i := 0; i < 3; i++ {
+			err := gardenClient.Destroy(container.Handle())
+			switch {
+			case err == nil:
+				// move on if Destroy succeeds
+				break
+			case strings.Contains(err.Error(), "unknown handle"):
+				// move on if container doesn't exist
+				break
+			case strings.Contains(err.Error(), "container already being destroyed"):
+				// move on if container is already being destroyed
+				break
+			case i == 2:
+				// record an error if Destroy failed 3 times
+				destroyContainerErrors = append(destroyContainerErrors, err)
+			default:
+				// try Destroy again otherwise
+				time.Sleep(50 * time.Millisecond)
 				continue
 			}
-
-			if strings.Contains(err.Error(), "container already being destroyed") {
-				continue
-			}
-
-			destroyContainerErrors = append(destroyContainerErrors, err)
 		}
 	}
 
