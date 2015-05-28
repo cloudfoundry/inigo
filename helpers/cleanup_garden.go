73593ab9ec09342056a2a8af764a0df6c7cea059
Amit Gupta and Matt Royal <agupta@pivotal.io>
2015-05-28 12:13:06 -0700

    Fix redundant destroy attempts in garden cleanup

--- helpers/cleanup_garden.go
+++ helpers/cleanup_garden.go
@@ -24,26 +24,26 @@ func CleanupGarden(gardenClient garden.Client) []error {
 
 		fmt.Fprintf(ginkgo.GinkgoWriter, "cleaning up container %s (%s)", container.Handle(), info.ContainerPath)
 
+	RETRY_LOOP:
 		// try to Destroy the container up to 3 times
 		for i := 0; i < 3; i++ {
 			err := gardenClient.Destroy(container.Handle())
 			switch {
 			case err == nil:
 				// move on if Destroy succeeds
-				break
+				break RETRY_LOOP
 			case strings.Contains(err.Error(), "unknown handle"):
 				// move on if container doesn't exist
-				break
+				break RETRY_LOOP
 			case strings.Contains(err.Error(), "container already being destroyed"):
 				// move on if container is already being destroyed
-				break
+				break RETRY_LOOP
 			case i == 2:
 				// record an error if Destroy failed 3 times
 				destroyContainerErrors = append(destroyContainerErrors, err)
 			default:
 				// try Destroy again otherwise
 				time.Sleep(50 * time.Millisecond)
-				continue
 			}
 		}
 	}
