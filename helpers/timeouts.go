306db25769faa92d63db50b4f88fde5437f07ac9
Nima Kaviani <nkavian@us.ibm.com>
2017-02-24 14:48:47 -0800

    Change default eventually timeout to 1 minute
    [#126989119]
    
    Signed-off-by: Chris Piraino <cpiraino@pivotal.io>

--- helpers/timeouts.go
+++ helpers/timeouts.go
@@ -7,7 +7,7 @@ import (
 	"github.com/onsi/gomega"
 )
 
-var DEFAULT_EVENTUALLY_TIMEOUT = 2 * time.Minute
+var DEFAULT_EVENTUALLY_TIMEOUT = 1 * time.Minute
 var DEFAULT_CONSISTENTLY_DURATION = 5 * time.Second
 
 func RegisterDefaultTimeouts() {
