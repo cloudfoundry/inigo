3567e463ad91573e64bebc6a5f28cdc0db176a06
Kai Xiang <kxiang@pivotal.io>
2017-08-18 12:09:17 -0700

    Add a inigo test for mounting volume with csi plugin
    [#149934735](https://www.pivotaltracker.com/story/show/149934735)

--- volman/volman_csi_test.go
+++ volman/volman_csi_test.go
@@ -1,9 +1,11 @@
 package volman_test
 
 import (
+	"path"
+
+	"github.com/jeffpak/local-node-plugin/node"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/jeffpak/local-node-plugin/node"
 )
 
 var _ = Describe("Given volman and a local-node-plugin", func() {
@@ -15,10 +17,24 @@ var _ = Describe("Given volman and a local-node-plugin", func() {
 			infos := listDriversResponse.Drivers
 			for _, info := range infos {
 				if info.Name == node.NODE_PLUGIN_ID {
-					return true;
+					return true
 				}
 			}
 			return false
-		}).Should(Equal(true));
+		}).Should(Equal(true))
+	})
+
+	It("should be able to mount volume", func() {
+		volumeName := "someVolume"
+		csiMountRootDir := "local-node-plugin-mount"
+		volumeConfig := map[string]interface{}{"volume_id": volumeName}
+
+		expectedMountPath := path.Join(componentMaker.VolmanDriverConfigDir, csiMountRootDir, node.NODE_PLUGIN_ID, volumeName)
+
+		mountPointResponse, err := volmanClient.Mount(logger, node.NODE_PLUGIN_ID, volumeName, volumeConfig)
+		Expect(err).NotTo(HaveOccurred())
+
+		mountPoint := mountPointResponse.Path
+		Expect(mountPoint).To(Equal(expectedMountPath))
 	})
 })
