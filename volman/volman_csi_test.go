298d2900d51f0395195ee566738f656fa5c145e4
Kai Xiang <kxiang@pivotal.io>
2017-08-23 16:49:55 -0700

    Add inigo test for unmount CSI volume
    [#149934809](https://www.pivotaltracker.com/story/show/149934809)

--- volman/volman_csi_test.go
+++ volman/volman_csi_test.go
@@ -3,6 +3,8 @@ package volman_test
 import (
 	"path"
 
+	"code.cloudfoundry.org/volman"
+
 	"github.com/jeffpak/local-node-plugin/node"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -24,17 +26,36 @@ var _ = Describe("Given volman and a local-node-plugin", func() {
 		}).Should(Equal(true))
 	})
 
-	It("should be able to mount volume", func() {
-		volumeName := "someVolume"
-		csiMountRootDir := "local-node-plugin-mount"
-		volumeConfig := map[string]interface{}{"volume_id": volumeName}
+	Context("when running mount and unmount", func() {
+		var (
+			volumeName        string
+			csiMountRootDir   string
+			expectedMountPath string
+			volumeConfig      map[string]interface{}
+			mountResponse     volman.MountResponse
+			err               error
+		)
 
-		expectedMountPath := path.Join(componentMaker.VolmanDriverConfigDir, csiMountRootDir, node.NODE_PLUGIN_ID, volumeName)
+		BeforeEach(func() {
+			volumeName = "someVolume"
+			csiMountRootDir = "local-node-plugin-mount"
+			volumeConfig = map[string]interface{}{"volume_id": volumeName}
+			expectedMountPath = path.Join(componentMaker.VolmanDriverConfigDir, csiMountRootDir, node.NODE_PLUGIN_ID, volumeName)
+		})
 
-		mountPointResponse, err := volmanClient.Mount(logger, node.NODE_PLUGIN_ID, volumeName, volumeConfig)
-		Expect(err).NotTo(HaveOccurred())
+		JustBeforeEach(func() {
+			mountResponse, err = volmanClient.Mount(logger, node.NODE_PLUGIN_ID, volumeName, volumeConfig)
+		})
 
-		mountPoint := mountPointResponse.Path
-		Expect(mountPoint).To(Equal(expectedMountPath))
+		It("should be able to mount volume", func() {
+			Expect(err).NotTo(HaveOccurred())
+			mountPoint := mountResponse.Path
+			Expect(mountPoint).To(Equal(expectedMountPath))
+		})
+
+		It("should be able to unmount volume", func() {
+			err = volmanClient.Unmount(logger, node.NODE_PLUGIN_ID, volumeName)
+			Expect(err).NotTo(HaveOccurred())
+		})
 	})
 })
