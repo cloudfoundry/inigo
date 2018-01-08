17e9af282aebf3e16a7ebd8541d0e6166584b34a
John Shahid <jvshahid@gmail.com>
2018-01-08 11:40:05 -0800

    Add assertions to ensure source and destination aren't empty
    Signed-off-by: Will Pragnell <wpragnell@pivotal.io>

--- helpers/copy.go
+++ helpers/copy.go
@@ -7,6 +7,8 @@ import (
 )
 
 func Copy(sourcePath, destinationPath string) {
+	Expect(sourcePath).NotTo(BeEmpty())
+	Expect(destinationPath).NotTo(BeEmpty())
 	err := exec.Command("cp", "-a", sourcePath, destinationPath).Run()
 	Expect(err).NotTo(HaveOccurred())
 }
