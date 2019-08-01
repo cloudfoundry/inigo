c7e6bf206fe6a4787585b8f3338158cb4c8def84
Amin Jamali <ajamali@pivotal.io>
2019-08-01 12:07:02 -0700

    Ensure  LRPStatePoller is backward compatible for v1.0.0 and v1.25.0
    Co-authored-by: Maria Shaldibina <mshaldibina@pivotal.io>

--- helpers/pollers.go
+++ helpers/pollers.go
@@ -1,6 +1,8 @@
 package helpers
 
 import (
+	"strings"
+
 	"code.cloudfoundry.org/bbs"
 	"code.cloudfoundry.org/bbs/models"
 	"code.cloudfoundry.org/lager"
@@ -62,28 +64,49 @@ func TaskFailedPoller(logger lager.Logger, client bbs.InternalClient, taskGuid s
 
 func LRPStatePoller(logger lager.Logger, client bbs.InternalClient, processGuid string, lrp *models.ActualLRP) func() string {
 	return func() string {
+		var foundLRP *models.ActualLRP
+
 		lrps, err := client.ActualLRPs(logger, models.ActualLRPFilter{ProcessGuid: processGuid})
-		Expect(err).NotTo(HaveOccurred())
-		if len(lrps) == 0 {
-			return ""
+		if err != nil && strings.Contains(err.Error(), "Invalid Response with status code: 404") {
+			lrpGroups, err := client.ActualLRPGroupsByProcessGuid(logger, processGuid)
+			Expect(err).NotTo(HaveOccurred())
+			if len(lrpGroups) == 0 {
+				return ""
+			}
+			foundLRP, _, err = lrpGroups[0].Resolve()
+			Expect(err).NotTo(HaveOccurred())
+		} else {
+			Expect(err).NotTo(HaveOccurred())
+			if len(lrps) == 0 {
+				return ""
+			}
+			foundLRP = lrps[0]
 		}
-		Expect(len(lrps)).To(BeNumerically(">", 0))
 		if lrp != nil {
-			*lrp = *lrps[0]
+			*lrp = *foundLRP
 		}
-		return lrps[0].State
+		return foundLRP.State
 	}
 }
 
 func LRPInstanceStatePoller(logger lager.Logger, client bbs.InternalClient, processGuid string, index int, lrp *models.ActualLRP) func() string {
 	return func() string {
 		i := int32(index)
+		var foundLRP *models.ActualLRP
 		lrps, err := client.ActualLRPs(logger, models.ActualLRPFilter{ProcessGuid: processGuid, Index: &i})
-		Expect(err).NotTo(HaveOccurred())
-		Expect(len(lrps)).To(Equal(1))
-		if lrp != nil {
-			*lrp = *lrps[0]
+		if err != nil && strings.Contains(err.Error(), "Invalid Response with status code: 404") {
+			lrpGroup, err := client.ActualLRPGroupByProcessGuidAndIndex(logger, processGuid, index)
+			Expect(err).NotTo(HaveOccurred())
+			foundLRP, _, err = lrpGroup.Resolve()
+			Expect(err).NotTo(HaveOccurred())
+		} else {
+			Expect(err).NotTo(HaveOccurred())
+			Expect(lrps).To(HaveLen(1))
+			foundLRP = lrps[0]
 		}
-		return lrps[0].State
+		if lrp != nil {
+			*lrp = *foundLRP
+		}
+		return foundLRP.State
 	}
 }
