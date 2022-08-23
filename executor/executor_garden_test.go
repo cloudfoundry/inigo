7a22f9bdf5c2c6bd2fd831294f0f75a0758821d3
Marc Paquette <mpaquette@vmware.com>
2022-08-23 19:40:03 +0000

    make container cmd consistent
    Signed-off-by: Josh Russett <jrussett@vmware.com>

--- executor/executor_garden_test.go
+++ executor/executor_garden_test.go
@@ -937,8 +937,7 @@ var _ = Describe("Executor/Garden", func() {
 
 					process, err := container.Run(garden.ProcessSpec{
 						Path: "sh",
-						// Args: []string{"-c", "mkdir some; echo hello > some/path"},
-						Args: []string{"-c", "echo kittens"},
+						Args: []string{"-c", `mkdir some; echo hello > some/path`},
 						User: "vcap",
 					}, garden.ProcessIO{})
 					Expect(err).NotTo(HaveOccurred())
