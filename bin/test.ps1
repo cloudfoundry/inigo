e7babdd04c7a570e8c56c4dea6efb62ff4d6f3da
Amin Jamali <ajamali@vmware.com>
2023-11-01 16:10:08 +0000

    Add logs.txt output to show progress

--- bin/test.ps1
+++ bin/test.ps1
@@ -135,6 +135,7 @@ $logsDir="$env:TMP/inigo-logs-$timestamp"
 echo "Log Dir: $logsDir"
 
 mkdir -Force "$logsDir"
+echo "Runing ginkgo in the background. Output will be at $logsDir/logs.txt"
 Invoke-Expression "go run github.com/onsi/ginkgo/v2/ginkgo $args --output-dir $logsDir --json-report report.json" | Out-File "$logsDir/logs.txt" -Encoding ASCII -Width 1000
 if ($LastExitCode -ne 0) {
   throw "tests failed"
